59814ac633b6b36495ceb79f4138f1314e580371
davisjoseph6 <davisjoseph767@email.com>
2024-04-11 01:25:34 +0200

    wrote a function that calculates the shape of a numpy.ndarray

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+a function that calculates the shape of a numpy.ndarray
+"""
+
+
+import numpy as np
+
+
+def np_shape(matrix):
+    """
+    Calculate the shape of a numpy.ndarray
+    """
+    return matrix.shape
