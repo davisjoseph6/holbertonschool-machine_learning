e5dfada449656d568f6a9de12c2ce463ebe50b1b
davisjoseph6 <davisjoseph767@email.com>
2024-04-11 02:21:40 +0200

    wrote a function that transposes matrix

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A function that transposes a matrix using numpy.
+"""
+
+
+def np_transpose(matrix):
+    """
+    Tranpose the given numpy.ndarray.
+    """
+    return matrix.T
