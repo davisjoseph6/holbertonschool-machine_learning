febdcde5b2e4f066896692ee6f3d2815d67aea74
davisjoseph6 <davisjoseph767@email.com>
2024-04-15 01:05:49 +0200

    fixed pycodestyle

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -24,13 +24,13 @@ def cat_matrices(mat1, mat2, axis=0):
     shape2 = get_shape(mat2)
 
     # Check if the matrices can be concatenated along the specified axis
-    if len(shape1) != len(shape2) or any(s1 != s2 for s1, s2 in zip(shape1[:axis], shape2[:axis])) \
-            or any(s1 != s2 for s1, s2 in zip(shape1[axis+1:], shape2[axis+1:])):
-                return None
+    if (len(shape1) != len(shape2) or
+            any(s1 != s2 for s1, s2 in zip(shape1[:axis], shape2[:axis])) or
+            any(s1 != s2 for s1, s2 in zip(shape1[axis+1:], shape2[axis+1:]))):
+        return None
 
     # Recursiveeeeeeeee concatenation of matrices
     if axis == 0:
         return mat1 + mat2
     else:
         return [cat_matrices(m1, m2, axis-1) for m1, m2 in zip(mat1, mat2)]
-
