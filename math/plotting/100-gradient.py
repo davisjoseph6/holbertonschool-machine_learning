c8b1e86c13aaa51789d82f9ba798e24a8d760b36
davisjoseph6 <davisjoseph767@email.com>
2024-04-18 09:30:25 +0200

    wrote a script to create a scatter plot of sampled elevations

--- math/plotting/100-gradient.py
+++ math/plotting/100-gradient.py
@@ -11,27 +11,11 @@ def gradient():
     y = np.random.randn(2000) * 10
     z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
 
-    # Generate coordinates and elevation data
-    x = np.random.randn(2000) * 10
-    y = np.random.randn(2000) * 10
-    # Elevation 
-    z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
-
-    # Create a scatter plot
-    plt.figure(figsize=(6.4, 4.8))
-    scatter = plt.scatter(x, y, c=z, cmap='viridis')  # Use the virids colormap
-
-    # Create a colorbar with a label
-    cbar = plt.colorbar(scatter)
-    cbar.set_label('elevation (m)')
-
-    # Labeling the axes and setting a title
-    plt.xlabel('x coordinate (m)')
-    plt.ylabel('y coordinate (m)')
-    plt.title('Mountain Elevation')
-
-    # Show the plot
+    plt.figure(figsize=(10, 6))  # Set the figure size
+    sc = plt.scatter(x, y, c=z, s=40, cmap='terrain')  # use a terrain colormap
+    cbar = plt.colorbar(sc, label="elevation (m")
+    cbar.set_label('elevation (m)', rotation=270, labelpad=20)
+    plt.title("Mountain Elevation")
+    plt.xlabel("x coordinate (m)")
+    plt.ylabel("y coordinate (m)")
     plt.show()
-
-
-
