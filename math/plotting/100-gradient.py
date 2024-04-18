c482e7a40c23387c04246f6c23c1205166607b85
davisjoseph6 <davisjoseph767@email.com>
2024-04-18 10:00:53 +0200

    wrote a script to create a scatter plot of sampled elevations

--- math/plotting/100-gradient.py
+++ math/plotting/100-gradient.py
@@ -10,10 +10,10 @@ def gradient():
     x = np.random.randn(2000) * 10
     y = np.random.randn(2000) * 10
     z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+    plt.figure(figsize=(6.4, 4.8))  # Set the figure size
 
-    plt.figure(figsize=(10, 6))  # Set the figure size
     sc = plt.scatter(x, y, c=z, s=40, cmap='terrain')  # use a terrain colormap
-    cbar = plt.colorbar(sc, label="elevation (m")
+    cbar = plt.colorbar(sc, label="elevation (m)")
     cbar.set_label('elevation (m)', rotation=270, labelpad=20)
     plt.title("Mountain Elevation")
     plt.xlabel("x coordinate (m)")
