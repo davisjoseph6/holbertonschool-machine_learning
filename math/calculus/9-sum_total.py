cdd5b0e87e9a445efa185232108cf8bee09003a8
davisjoseph6 <davisjoseph767@email.com>
2024-04-19 13:48:41 +0200

    fixed pycodestyle

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -10,11 +10,11 @@ def summation_i_squared(n):
     Parameters:
     n (int): The number up to which the square of numbers
     are summed
-    
+
     Returns:
     int: The sum of squares from 1 to n
     None: If n is not a valid integer
     """
-    if not isinstance(n, int) or n < 1:  # Validate that n is a positive integer
+    if not isinstance(n, int) or n < 1:  # Validate n is a positive integer
         return None
-    return (n * (n +1) * (2 * n + 1)) // 6  # Calculate and return the sum 
+    return (n * (n + 1) * (2 * n + 1)) // 6  # Calculate and return the sum
