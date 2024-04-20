604d244e5da351b81784a96b252fc82dd4b07295
davisjoseph6 <davisjoseph767@email.com>
2024-04-20 02:26:28 +0200

    modified the function

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python3
 """
-A function that calculates the integral of a polynomial
+integration functions
 """
 
 
 def poly_integral(poly, C=0):
-    """
-    Validate input
-    """
-    if not isinstance(poly, list) or not all(isinstance(x, (int, float)) for x in poly):
-        return None
-
-    if not isinstance(C, (int, float)):
-        return None
-
-    if not poly or all(x == 0 for x in poly):
-        return [C] if C != 0 else [0]
-
-    arr = [C] + [poly[i] / (i + 1) for i in range(len(poly))]
-
-    return [int(x) if isinstance(x, float) and x.is_integer() else x for x in arr]
+    """function that calculates the integral of a polynomial"""
+    if poly and isinstance(poly, list) and (
+            isinstance(C, int) or isinstance(C, float)) and all(
+            isinstance(x, (int, float)) for x in poly):
+        if poly == [0]:
+            return [C]
+        arr = [float(C)] + [poly[i] / (i + 1) for i in range(0, len(poly))]
+        return [float(x) if not x.is_integer() else int(x) for x in arr]
+    return None
