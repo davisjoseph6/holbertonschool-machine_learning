8c5c1b5d6e95d07d36e39f676154536691c06e55
davisjoseph6 <davisjoseph767@email.com>
2024-04-22 23:09:46 +0200

    fixed pycodestyle

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -2,8 +2,10 @@
 
 import numpy as np
 
+
 class Node:
-    def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
+    def __init__(self, feature=None, threshold=None, left_child=None,
+                 right_child=None, is_root=False, depth=0):
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -13,21 +15,22 @@ class Node:
         self.sub_population = None
         self.depth = depth
 
-    def max_depth_below(self) :
+    def max_depth_below(self):
         # Initialize max_depth with the current node's depth
         max_depth = self.depth
 
-        # If the node has a left child, calculate the maximum depth below 
+        # If the node has a left child, calculate the maximum depth below
         # the left child
         if self.left_child is not None:
             max_depth = max(max_depth, self.left_child.max_depth_below())
 
-        # If the node has a right child, calculate the maximum depth below 
+        # If the node has a right child, calculate the maximum depth below
         # the right child
         if self.right_child is not None:
             max_depth = max(max_depth, self.right_child.max_depth_below())
 
         return max_depth
+
 
 class Leaf(Node):
     def __init__(self, value, depth=None):
@@ -36,11 +39,13 @@ class Leaf(Node):
         self.is_leaf = True
         self.depth = depth
 
-    def max_depth_below(self) :
+    def max_depth_below(self):
         return self.depth
 
+
 class Decision_Tree():
-    def __init__(self, max_depth=10, min_pop=1, seed=0, split_criterion="random", root=None):
+    def __init__(self, max_depth=10, min_pop=1, seed=0,
+                 split_criterion="random", root=None):
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
@@ -53,5 +58,5 @@ class Decision_Tree():
         self.split_criterion = split_criterion
         self.predict = None
 
-    def depth(self) :
+    def depth(self):
         return self.root.max_depth_below()
