051808361e9ab028734eeed4e91ae7976dc8c00b
davisjoseph6 <davisjoseph767@email.com>
2024-05-01 04:25:40 +0200

    fixed pycodestyle

--- supervised_learning/classification/3-neuron.py
+++ supervised_learning/classification/3-neuron.py
@@ -63,5 +63,6 @@ class Neuron:
         Calculates the cost of the model using logistic regression
         """
         m = Y.shape[1]
-        cost = -(1 / m) * np.sum(Y * np.log(A) + (1 -Y) * np.log(1.0000001 - A))
+        cost = -(1 / m) * np.sum(Y * np.log(A) +
+                                 (1 - Y) * np.log(1.0000001 - A))
         return cost
