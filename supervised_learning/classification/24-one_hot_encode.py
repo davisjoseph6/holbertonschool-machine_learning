410767f8a13ab97d9caa50e28e4a47ca5c585b3f
davisjoseph6 <davisjoseph767@email.com>
2024-05-04 21:19:52 +0200

    a function that converts a numeric label vector into a one-hot matrix

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """
+    Converts a numeric label vector Y into a one-hot matrix.
+    """
+    if not isinstance(Y, np.ndarray) or len(Y) == 0:
+        return None
+    if not isinstance(classes, int) or classes <= 0:
+        return None
+
+    try:
+        one_hot = np.eye(classes)[Y].T
+        return one_hot
+    except IndexError as e:
+        return None
