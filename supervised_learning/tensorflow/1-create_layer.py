d8850ba8928ca728c4100ff35e184e501c7828b8
davisjoseph6 <davisjoseph767@email.com>
2024-05-12 06:46:26 +0200

    fixed pycodestyle

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -6,12 +6,13 @@ Creates a new layer for a neural network
 import tensorflow.compat.v1 as tf
 tf.disable_eager_execution()
 
+
 def create_layer(prev, n, activation):
     """
     Creates a new layer for a neural network
     """
     initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
     layer = tf.layers.Dense(units=n, activation=activation,
-            kernel_initializer=initializer,
-            name='layer')
+                            kernel_initializer=initializer,
+                            name='layer')
     return layer(prev)
