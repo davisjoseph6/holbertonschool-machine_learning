cd6c7204648ae2bcf77e72069b89424607a917ca
davisjoseph6 <davisjoseph767@email.com>
2024-05-17 05:13:59 +0200

    updated

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    Updates a variable using the gradient descent with momentum optimization
+    algorithm.
+    """
+    v = beta1 * v + (1 - beta1) * grad
+    var = var - alpha * v
+    return var, v
