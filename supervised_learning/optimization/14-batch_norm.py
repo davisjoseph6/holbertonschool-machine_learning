3830c3c3553051575f5bca6818398a0219020d28
davisjoseph6 <davisjoseph767@email.com>
2024-05-19 01:07:51 +0200

    updated

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -11,13 +11,14 @@ def create_batch_norm_layer(prev, n, activation):
     Creates a batch normalization layer for a neural network in TensorFlow.
 
     Args:
-        prev (tensorflow.Tensor): The activated output of the previous layer.
-        n (int): The number of nodes in the layer to be created.
+        prev (tensorflow.Tensor: The activated output of the previpus layer.
+        n (init): The number of nodes in the layer to be created.
         activation: The activation function that should be used on the output
         of the layer.
 
     Returns:
-        tensorflow.Tensor: A tensor of the activated output for the layer.
+        tensorflow.Tensor: A tensor of the activated output for the layers.
+    Autor: davisjoseph6
     """
     # He et. al initialization
     initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
@@ -43,11 +44,11 @@ def create_batch_norm_layer(prev, n, activation):
 
     # apply batch normalization to Z
     Z_batch_norm = tf.nn.batch_normalization(
-        x=Z,
-        mean=mean,
-        variance=variance,
-        offset=beta,
-        scale=gamma,
-        variance_epsilon=epsilon)
+            x=Z,
+            mean=mean,
+            variance=variance,
+            offset=beta,
+            scale=gamma,
+            variance_epsilon=epsilon)
 
     return activation(Z_batch_norm)
