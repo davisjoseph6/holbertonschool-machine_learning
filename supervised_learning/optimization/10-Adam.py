93082fb076e6a99d521896307256e5408efac268
davisjoseph6 <davisjoseph767@email.com>
2024-05-18 21:00:58 +0200

    updated code

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -10,5 +10,6 @@ def create_Adam_op(alpha, beta1, beta2, epsilon):
     """
     Sets up the Adam optimization algorithm in TensorFlow.
     """
-    optimizer = tf.keras.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2, epsilon=epsilon)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=alpha, beta_1=beta1,
+                                         beta_2=beta2, epsilon=epsilon)
     return optimizer
