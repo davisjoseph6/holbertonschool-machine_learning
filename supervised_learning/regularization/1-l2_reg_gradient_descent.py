b71a4e270971c7e976bbb35f7ce713f678a29ada
davisjoseph6 <davisjoseph767@email.com>
2024-05-26 00:50:24 +0200

    updated the weights and biases

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -17,17 +17,17 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
 
     dZ = A_L - Y
 
-    for l in range(L, 0, -1):
-        A_prev = cache['A' + str(l - 1)]
-        W = weights['W' + str(l)]
-        b = weights['b' + str(l)]
+    for layer in range(L, 0, -1):
+        A_prev = cache['A' + str(layer - 1)]
+        W = weights['W' + str(layer)]
+        b = weights['b' + str(layer)]
 
         dW = (1 / m) * np.matmul(dZ, A_prev.T) + (lambtha / m) * W
         db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
-        weights['W' + str(l)] = W - alpha * dW
-        weights['b' + str(l)] = b - alpha * db
+        weights['W' + str(layer)] = W - alpha * dW
+        weights['b' + str(layer)] = b - alpha * db
 
-        if l > 1:
+        if layer > 1:
             dA_prev = np.matmul(W.T, dZ)
             dZ = dA_prev * (1 - A_prev ** 2)
