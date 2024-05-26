5fb9f48abcd3d0023e819b399f33248d15e568a8
davisjoseph6 <davisjoseph767@email.com>
2024-05-26 22:12:16 +0200

    dropout

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python3
 """
-Creates a layer of a neural netowrk using dropout.
+Create a Layer with Dropout
 """
-
 import tensorflow as tf
 
 
 def dropout_create_layer(prev, n, activation, keep_prob, training=True):
     """
-    Creates a layer of a neural network using dropout.
+    Creates a dense layer with dropout.
+
+    Parameters:
+    - prev: tensor containing the output of the previous layer
+    - n: number of nodes the new layer should contain
+    - activation: activation function for the new layer
+    - keep_prob: probability that a node will be kept
+    - training: boolean indicating whether the model is in training mode
+
+    Returns: the output of the new layer
     """
-    # Create a dense layer with n units and the specified activation function
-    dense_layer = tf.keras.layers.Dense(units=n, activation=activation)(prev)
+    # Weight initialization: He et al.
+    init_weights = tf.keras.initializers.VarianceScaling(
+        scale=2.0, mode="fan_avg")
 
-    # Apply dropout to the dense layer
-    dropout_layer = tf.keras.layers.Dropout(rate=1-keep_prob)(dense_layer, training=training)
+    layer = tf.keras.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=init_weights
+    )
 
-    return dropout_layer
+    # Apply the dense layer
+    output = layer(prev)
+
+    # Apply dropout on output only during training
+    if training:
+        dropout = tf.keras.layers.Dropout(rate=(1 - keep_prob))
+        output = dropout(output, training=training)
+
+    return output
