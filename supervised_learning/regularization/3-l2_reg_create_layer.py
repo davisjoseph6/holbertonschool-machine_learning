9e348e345c1dfca993ac6427776452b2666353c1
davisjoseph6 <davisjoseph767@email.com>
2024-05-26 18:10:53 +0200

    fixed pycodestyle

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -5,6 +5,7 @@ Create a Layer with L2 Regularization.
 
 import tensorflow as tf
 
+
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """
     Creates a neural network layer with L2 regularization.
@@ -19,7 +20,9 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     tensor: the output of the new layer.
     """
     # Weight initialization: He et. al
-    init_weights = tf.keras.initializers.VarianceScaling(scale=2.0, mode="fan_avg")
+    init_weights = tf.keras.initializers.VarianceScaling(
+            scale=2.0, mode="fan_avg"
+            )
     # Regularization for this layer's loss: L2 regularizer
     l2_regularizer = tf.keras.regularizers.L2(lambtha)
 
