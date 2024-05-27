599e3d193b0d2d84d778a7b11e17c88909db52af
davisjoseph6 <davisjoseph767@email.com>
2024-05-27 22:43:15 +0200

    added documentation

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -9,6 +9,19 @@ import tensorflow.keras as K
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
     Builds a neural network using the Keras library with the Functional API
+
+    Args:
+        nx (int): The number of input features to the network
+        layers (list): A list containing the number of nodes in each layer
+        of the network.
+        activations (list): A list containing the activation functions used
+        for each layer of the network
+        lambtha (float): The L2 regularization parameter.
+        keep_prob (float): The probability that a node will be kept for
+        dropout
+
+    Returns:
+    K.Model: The constructed Keras model
     """
     inputs = K.Input(shape=(nx,))
     x = inputs
