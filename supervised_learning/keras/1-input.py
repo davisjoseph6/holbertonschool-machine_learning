fc142a232cec3098051ca3657abd4130b826f41e
davisjoseph6 <davisjoseph767@email.com>
2024-05-27 22:38:46 +0200

    a function that builds a neural netowrk with the Keras

---
+++ supervised_learning/keras/1-input.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Builds a neural network using the Keras library with the Functional API
+"""
+
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    Builds a neural network using the Keras library with the Functional API
+    """
+    inputs = K.Input(shape=(nx,))
+    x = inputs
+
+    for i in range(len(layers)):
+        x = K.layers.Dense(layers[i],
+                           activation=activations[i],
+                           kernel_regularizer=K.regularizers.l2(lambtha))(x)
+        if i < len(layers) - 1:
+            x = K.layers.Dropout(1 - keep_prob)(x)
+
+    model = K.Model(inputs=inputs, outputs=x)
+    return model
