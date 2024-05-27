828a93c459bc7826503057ed5902e22d5974e20d
davisjoseph6 <davisjoseph767@email.com>
2024-05-27 22:16:47 +0200

    imported keras as K

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
-import tensorflow as tf
-from tensorflow.keras import layers, models, regularizers
+import tensorflow.keras as K
 
 
-def build_model(nx, layers_list, activations, lambtha, keep_prob):
-    model = models.Sequential()
-    for i in range(len(layers_list)):
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    model = K.Sequential()
+    for i in range(len(layers)):
         if i == 0:
-            model.add(layers.Dense(layers_list[i], input_shape=(nx,), activation=activations[i],
-                kernel_regularizer=regularizers.l2(lambtha)))
+            model.add(K.layers.Dense(layers[i], input_shape=(nx,),
+                activation=activations[i], kernel_regularizer=K.regularizers.
+                l2(lambtha)))
         else:
-            model.add(layers.Dense(layers_list[i], activation=activations[i],
-                kernel_regularizer=regularizers.l2(lambtha)))
-        if i < len(layers_list) - 1:  # Don't add dropput after the last layer
-            model.add(layers.Dropout(1 - keep_prob))
+            model.add(K.layers.Dense(layers[i], activation=activations[i],
+                kernel_regularizer=K.regularizers.l2(lambtha)))
+        if i < len(layers) - 1:
+            model.add(K.layers.Dropout(1 - keep_prob))
     return model
