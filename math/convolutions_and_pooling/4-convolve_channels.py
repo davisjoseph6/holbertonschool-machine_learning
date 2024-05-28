5ddf5bd74b811ea636cbfdc932d0d1512910bae4
davisjoseph6 <davisjoseph767@email.com>
2024-05-29 00:21:50 +0200

    wrote a function that performs a convolution on images with channels

---
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Performs a convolution on images with channels
+"""
+
+import numpy as np
+
+
+def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+    """
+    Performs a convolution on images with channels
+    """
+    m, h, w, c = images.shape
+    kh, kw, kc = kernel.shape
+    sh, sw = stride
+
+    if isinstance(padding, tuple):
+        ph, pw = padding
+    elif padding == 'same':
+        ph = ((h - 1) * sh + kh - h) // 2 + 1
+        pw = ((w - 1) * sw + kw - w) // 2 + 1
+    elif padding == 'valid':
+        ph = 0
+        pw = 0
+
+    # Calculate output dimensions
+    output_h = (h + 2 * ph - kh) // sh + 1
+    output_w = (w + 2 * pw - kw) // sw + 1
+
+    # Pad the images with zeros
+    padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant')
+
+    # Initialize convolution output array
+    convolved = np.zeros((m, output_h, output_w))
+
+    for i in range(output_h):
+        for j in range(output_w):
+            # Extract region from padded images, scaling indexes by stride
+            region = padded_images[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
+            # Convolve each image (m) for this region (i, j)
+            convolved[:, i, j] = np.sum(region * kernel, axis=(1, 2, 3))
+
+    return convolved
