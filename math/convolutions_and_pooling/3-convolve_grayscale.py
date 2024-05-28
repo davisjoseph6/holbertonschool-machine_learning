fce452d8275b27862c1901f3ce64fad6fe6f9ac5
davisjoseph6 <davisjoseph767@email.com>
2024-05-29 00:05:26 +0200

    updated

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Performs a convolution on grayscale images
+Strided Convolution
 """
 
 import numpy as np
@@ -10,36 +10,35 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     """
     Performs a convolution on grayscale images.
     """
+    # Setup matrixes and padding dimensions
     m, h, w = images.shape
     kh, kw = kernel.shape
     sh, sw = stride
 
-    if padding == 'same':
-        ph = (kh - 1) // 2
-        pw = (kw - 1) // 2
-        output_h = h
-        output_w = w
+    if isinstance(padding, tuple):
+        ph, pw = padding
+    elif padding == 'same':
+        ph = ((h - 1) * sh + kh - h) // 2 + 1
+        pw = ((w - 1) * sw + kw - w) // 2 + 1
     elif padding == 'valid':
         ph = 0
         pw = 0
-        output_h = (h - kh) // sh + 1
-        output_w = (w - kw) // sw + 1
-    else:
-        ph, pw = padding
-        output_h = (h + 2 * ph - kh) // sh + 1
-        output_w = (w + 2 * pw - kw) // sw + 1
+    
+    # Calculate output dimensions
+    output_h = (h + 2 * ph - kh) // sh + 1
+    output_w = (w + 2 * pw - kw) // sw + 1
 
-    # Pad the images with zeros
-    padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant', constant_values=0)
+    # Pading indexes: (before, after), shortcut is (padding,)
+    padded_imgs = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant',)
 
-    # Initialize the output array with zeros
-    output = np.zeros((m, output_h, output_w))
+    # Initialize convolution output array
+    convolved = np.zeros((m, output_h, output_w))
 
     for i in range(output_h):
         for j in range(output_w):
-            # Apply the kernel to each position of the padded image
-            output[:, i, j] = np.sum(
-                    padded_images[:, i*sh:i*sh+kh, j*sw:j*sw+kw] * kernel, axis=(1, 2)
-                    )
+            # Extract region from padded images, scaling indexes by stride
+            region = padded_imgs[:, i*sh:i*sh+kh, j*sw:j*sw+kw]
+            # Convolve each image (m) for this region (i, j)
+            convolved[:, i, j] = np.sum(region * kernel, axis=(1, 2))
 
-    return output
+    return convolved
