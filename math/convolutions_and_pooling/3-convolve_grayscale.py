6e22315f3b6fd88e39298687b83b353aaef7d833
davisjoseph6 <davisjoseph767@email.com>
2024-05-28 23:49:43 +0200

    wrote a function

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -17,18 +17,20 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     if padding == 'same':
         ph = (kh - 1) // 2
         pw = (kw - 1) // 2
+        output_h = h
+        output_w = w
     elif padding == 'valid':
         ph = 0
         pw = 0
+        output_h = (h - kh) // sh + 1
+        output_w = (w - kw) // sw + 1
     else:
         ph, pw = padding
+        output_h = (h + 2 * ph - kh) // sh + 1
+        output_w = (w + 2 * pw - kw) // sw + 1
 
     # Pad the images with zeros
     padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant', constant_values=0)
-
-    # Calculate the dimensions of the output
-    output_h = (h + 2 * ph - kh) // sh + 1
-    output_w = (w + 2 * pw - kw) // sw + 1
 
     # Initialize the output array with zeros
     output = np.zeros((m, output_h, output_w))
