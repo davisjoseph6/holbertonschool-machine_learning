2b08603c826a6c7883d5004cbcdbc92400ba330c
davisjoseph6 <davisjoseph767@email.com>
2024-06-03 21:17:09 +0200

    updated

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -1,66 +1,52 @@
 #!/usr/bin/env python3
-"""
-    Convolutional Forward Propagation
-"""
-
 import numpy as np
-
 
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     """
-        function that performs forward propagation over a conv layer of NN
-
-        :param A_prev: ndarray, shape(m,h_prev,w_prev,c_prev) output layer
-        :param W: ndarray, shape(kh,kw,c_prev,c_new) kernel
-        :param b: ndarray, shape(1,1,1,c_new) biases
-        :param activation: activation function
-        :param padding: string 'same' or 'valid'
-        :param stride: tuple (sh,sw)
-
-        :return: output of the convolutional layer
+    Perform forward propagation over a convolutional layer of a neural network.
+    
+    Parameters:
+    - A_prev (numpy.ndarray): output of the previous layer with shape (m, h_prev, w_prev, c_prev)
+    - W (numpy.ndarray): kernels for the convolution with shape (kh, kw, c_prev, c_new)
+    - b (numpy.ndarray): biases applied to the convolution with shape (1, 1, 1, c_new)
+    - activation (function): activation function applied to the convolution
+    - padding (str): 'same' or 'valid', indicating the type of padding used
+    - stride (tuple): (sh, sw) containing the strides for the convolution
+    
+    Returns:
+    - numpy.ndarray: the output of the convolutional layer
     """
-    # size output layer, kernel, stride
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw, _, c_new = W.shape
     sh, sw = stride
 
-    # output size and padding
-    if padding == 'valid':
-        # no padding
+    if padding == "same":
+        ph = int(((h_prev - 1) * sh + kh - h_prev) / 2)
+        pw = int(((w_prev - 1) * sw + kw - w_prev) / 2)
+    elif padding == "valid":
         ph, pw = 0, 0
-    elif padding == 'same':
-        ph = int((((h_prev - 1) * sh + kh - h_prev) / 2))
-        pw = int((((w_prev - 1) * sw + kw - w_prev) / 2))
+    else:
+        raise ValueError("Padding must be 'same' or 'valid'")
 
-    # output size
-    output_height = int((h_prev - kh + 2 * ph) / sh + 1)
-    output_width = int((w_prev - kw + 2 * pw) / sw + 1)
+    h_new = int((h_prev - kh + 2 * ph) / sh + 1)
+    w_new = int((w_prev - kw + 2 * pw) / sw + 1)
 
-    # initialize output
-    convolved_images = np.zeros((m, output_height, output_width, c_new))
+    A_prev_padded = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant', constant_values=0)
+    
+    Z = np.zeros((m, h_new, w_new, c_new))
 
-    # pad image
-    image_pad = np.pad(A_prev,
-                       ((0, 0), (ph, ph),
-                        (pw, pw), (0, 0)), mode='constant')
+    for i in range(h_new):
+        for j in range(w_new):
+            for k in range(c_new):
+                vert_start = i * sh
+                vert_end = vert_start + kh
+                horiz_start = j * sw
+                horiz_end = horiz_start + kw
 
-    # convolution
-    for k in range(c_new):
-        for h in range(output_height):
-            for w in range(output_width):
-                # extract region from each image
-                image_zone = image_pad[:, h * sh:h * sh + kh,
-                                       w * sw:w * sw + kw, :]
-
-                # element wize multiplication
-                convolved_images[:, h, w, k] = np.sum(image_zone
-                                                      * W[:, :, :, k],
-                                                      axis=(1, 2, 3))
-
-    # add bias
-    Z = convolved_images + b
-
-    # apply activation function
-    Z = activation(Z)
-
-    return Z
+                A_slice = A_prev_padded[:, vert_start:vert_end, horiz_start:horiz_end, :]
+                Z[:, i, j, k] = np.sum(A_slice * W[:, :, :, k], axis=(1, 2, 3))
+    
+    Z = Z + b
+    A = activation(Z)
+    
+    return A
