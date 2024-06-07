4e0c49e48bcf7e73f6767f4846ef257a0660fb6c
davisjoseph6 <davisjoseph767@email.com>
2024-06-07 23:09:57 +0200

    fixed pycodestyle

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -23,14 +23,16 @@ def lenet5(X):
 
     # Convolutional layer with 6 kernels of shape 5x5 with same padding
     conv1 = K.layers.Conv2D(filters=6, kernel_size=(5, 5), padding='same',
-            activation='relu', kernel_initializer=initializer)(X)
+                            activation='relu',
+                            kernel_initializer=initializer)(X)
 
     # Max pooling layer with kernels of shape 2x2 with 2x2 strides
     pool1 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
 
     # Convolutional layer with 16 kernels of shape 5x5 with valid padding
     conv2 = K.layers.Conv2D(filters=16, kernel_size=(5, 5), padding='valid',
-            activation='relu', kernel_initializer=initializer)(pool1)
+                            activation='relu',
+                            kernel_initializer=initializer)(pool1)
 
     # Max pooling layer with kernels of shape 2x2 with 2x2 strides
     pool2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
@@ -39,18 +41,22 @@ def lenet5(X):
     flatten = K.layers.Flatten()(pool2)
 
     # Fully connected layer with 120 nodes
-    fc1 = K.layers.Dense(units=120, activation='relu', kernel_initializer=initializer)(flatten)
+    fc1 = K.layers.Dense(units=120, activation='relu',
+                         kernel_initializer=initializer)(flatten)
 
     # Fully connected layer with 84 nodes
-    fc2 = K.layers.Dense(units=84, activation='relu', kernel_initializer=initializer)(fc1)
+    fc2 = K.layers.Dense(units=84, activation='relu',
+                         kernel_initializer=initializer)(fc1)
 
     # Fully connected softmax output layer with 10 nodes
-    output = K.layers.Dense(units=10, activation='softmax', kernel_initializer=initializer)(fc2)
+    output = K.layers.Dense(units=10, activation='softmax',
+                            kernel_initializer=initializer)(fc2)
 
     # Create the model
     model = K.Model(inputs=X, outputs=output)
 
     # Compile the model to use Adam optimization and accuracy metrics
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='categorical_crossentropy',
+                  metrics=['accuracy'])
 
     return model
