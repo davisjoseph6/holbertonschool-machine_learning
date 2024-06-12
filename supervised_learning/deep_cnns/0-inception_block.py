ebffed9a47224775358ae6ac97ca56be77941c45
davisjoseph6 <davisjoseph767@email.com>
2024-06-13 00:27:53 +0200

    fixed pycodestyle

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -32,21 +32,29 @@ def inception_block(A_prev, filters):
     F1, F3R, F3, F5R, F5, FPP = filters
 
     # 1x1 convolution branch
-    conv_1x1 = K.layers.Conv2D(F1, (1, 1), padding='same', activation='relu')(A_prev)
+    conv_1x1 = K.layers.Conv2D(
+            F1, (1, 1), padding='same', activation='relu')(A_prev)
 
     # 1x1 convolution before 3x3 convolution branch
-    conv_3x3_reduce = K.layers.Conv2D(F3R, (1, 1), padding='same', activation='relu')(A_prev)
-    conv_3x3 = K.layers.Conv2D(F3, (3, 3), padding='same', activation='relu')(conv_3x3_reduce)
+    conv_3x3_reduce = K.layers.Conv2D(
+            F3R, (1, 1), padding='same', activation='relu')(A_prev)
+    conv_3x3 = K.layers.Conv2D(
+            F3, (3, 3), padding='same', activation='relu')(conv_3x3_reduce)
 
     # 1x1 convolution before 5x5 convolution branch
-    conv_5x5_reduce = K.layers.Conv2D(F5R, (1, 1), padding='same', activation='relu')(A_prev)
-    conv_5x5 = K.layers.Conv2D(F5, (5, 5), padding='same', activation='relu')(conv_5x5_reduce)
+    conv_5x5_reduce = K.layers.Conv2D(
+            F5R, (1, 1), padding='same', activation='relu')(A_prev)
+    conv_5x5 = K.layers.Conv2D(
+            F5, (5, 5), padding='same', activation='relu')(conv_5x5_reduce)
 
     # 3x3 max pooling before 1x1 convolution branch
-    max_pool = K.layers.MaxPooling2D((3, 3), strides=(1, 1), padding='same')(A_prev)
-    max_pool_conv = K.layers.Conv2D(FPP, (1, 1), padding='same', activation='relu')(max_pool)
+    max_pool = K.layers.MaxPooling2D(
+            (3, 3), strides=(1, 1), padding='same')(A_prev)
+    max_pool_conv = K.layers.Conv2D(
+            FPP, (1, 1), padding='same', activation='relu')(max_pool)
 
     # Concatenate all branches
-    output = K.layers.Concatenate(axis=-1)([conv_1x1, conv_3x3, conv_5x5, max_pool_conv])
+    output = K.layers.Concatenate(axis=-1)(
+            [conv_1x1, conv_3x3, conv_5x5, max_pool_conv])
 
     return output
