364ae31df344b51d59054d681d0dc8637b7e3919
davisjoseph6 <davisjoseph767@email.com>
2024-06-13 02:32:38 +0200

    fix

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -8,18 +8,7 @@ import tensorflow.keras as K
 
 def identity_block(A_prev, filters):
     """
-    Builds an identity block as described in
-    'Deep Residual Learning for Image Recognition' (2015).
-
-    Arguments:
-    - A_prev is the output from the previous layer
-    - filters is a tuple or list containing F11, F3, F12, respectively:
-       - F11 is the number of filters in the first 1x1 convolution
-       - F3 is the number of filters in the 3x3 convolution
-       - F12 is the number of filters in the second 1x1 convolution
-
-    Returns:
-    - The activated output of the identity block
+    Builds an identity block as described
     """
     F11, F3, F12 = filters
     he_normal = K.initializers.he_normal(seed=0)
