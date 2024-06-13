6f30247e63986f6ac7b539cb92d2a66b3d6f7e3e
davisjoseph6 <davisjoseph767@email.com>
2024-06-13 05:34:53 +0200

    wrote a function that builds a transition layer

---
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Transition Layer
+"""
+
+from tensorflow import keras as K
+
+
+def transition_layer(X, nb_filters, compression):
+    """
+    Builds a transition layer as described in 'Densely Connected Convolutional Networks' (2016).
+
+    Parameters:
+    X : tensor
+        The output of the previous layer.
+    nb_filters : int
+        The number of filters in X.
+    compression : float
+        The compression factor for the transition layer.
+
+    Returns:
+    tensor, int
+        The output of the transition layer and the number of filters within the output, respectively.
+    """
+    init = K.initializers.HeNormal(seed=0)
+    compressed_filters = int(nb_filters * compression)
+
+    # Batch Normalization and ReLU
+    bn = K.layers.BatchNormalization(axis=-1)(X)
+    relu = K.layers.Activation('relu')(bn)
+
+    # 1x1 Convolution
+    conv = K.layers.Conv2D(compressed_filters, (1, 1), padding='same', kernel_initializer=init)(relu)
+
+    # Average Pooling
+    avg_pool = K.layers.AveragePooling2D((2, 2), strides=(2, 2), padding='same')(conv)
+
+    return avg_pool, compressed_filters
