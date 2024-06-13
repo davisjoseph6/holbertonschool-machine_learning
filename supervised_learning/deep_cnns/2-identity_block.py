d965b35e8acc1127fcfb8c9918dcd9a1c96dea4f
davisjoseph6 <davisjoseph767@email.com>
2024-06-13 02:35:54 +0200

    updated

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -3,36 +3,42 @@
 Identity block module
 """
 
-import tensorflow.keras as K
-
+from tensorflow import keras as K
 
 def identity_block(A_prev, filters):
     """
-    Builds an identity block as described
+    Builds an identity block as described in 'Deep Residual Learning for Image Recognition' (2015).
+
+    Arguments:
+    - A_prev is the output from the previous layer
+    - filters is a tuple or list containing F11, F3, F12, respectively:
+        - F11 is the number of filters in the first 1x1 convolution
+        - F3 is the number of filters in the 3x3 convolution
+        - F12 is the number of filters in the second 1x1 convolution
+
+    Returns:
+    - The activated output of the identity block
     """
     F11, F3, F12 = filters
-    he_normal = K.initializers.he_normal(seed=0)
+    initializer = K.initializers.he_normal(seed=0)
 
-    # First component of the main path
-    conv1 = K.layers.Conv2D(
-            F11, (1, 1), padding='same', kernel_initializer=he_normal)(A_prev)
+    # First 1x1 convolution
+    conv1 = K.layers.Conv2D(F11, (1, 1), padding='same', kernel_initializer=initializer)(A_prev)
     bn1 = K.layers.BatchNormalization(axis=3)(conv1)
     act1 = K.layers.Activation('relu')(bn1)
 
-    # Second component of the main path
-    conv2 = K.layers.Conv2D(
-            F3, (3, 3), padding='same', kernel_initializer=he_normal)(act1)
+    # 3x3 convolution
+    conv2 = K.layers.Conv2D(F3, (3, 3), padding='same', kernel_initializer=initializer)(act1)
     bn2 = K.layers.BatchNormalization(axis=3)(conv2)
     act2 = K.layers.Activation('relu')(bn2)
 
-    # Third component of the main path
-    conv3 = K.layers.Conv2D(
-            F12, (1, 1), padding='same', kernel_initializer=he_normal)(act2)
+    # Second 1x1 convolution
+    conv3 = K.layers.Conv2D(F12, (1, 1), padding='same', kernel_initializer=initializer)(act2)
     bn3 = K.layers.BatchNormalization(axis=3)(conv3)
 
-    # Add shortcut value to the main path,
-    # and pass it through a RELU activation
+    # Adding the input tensor to the output of the block
     add = K.layers.Add()([bn3, A_prev])
-    act3 = K.layers.Activation('relu')(add)
+    output = K.layers.Activation('relu')(add)
 
-    return act3
+    return output
+
