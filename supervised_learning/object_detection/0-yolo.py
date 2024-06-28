c2722abdf530177ef34d9b0f5d43adc3871b5f30
davisjoseph6 <davisjoseph767@email.com>
2024-06-28 03:11:32 +0200

    updated

---
+++ supervised_learning/object_detection/0-yolo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Initialize the Yolo class with the specified parameters.
+"""
+
+import tensorflow.keras as K
+import numpy as np
+
+class Yolo:
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """
+        Initialize the Yolo class with the specified parameters
+        """
+        self.model = K.models.load_model(model_path)
+        self.class_names = self._load_class_names(classes_path)
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
+
+    def _load_class_names(self, classes_path):
+        """
+        Load class names from a file.
+        """
+        with open(classes_path, 'r') as file:
+            class_names = file.read().strip().split('\n')
+        return class_names
