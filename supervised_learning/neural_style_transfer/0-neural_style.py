c89692a8d628717ba781d0840b4919debd3c4cad
davisjoseph6 <davisjoseph767@email.com>
2024-07-05 08:39:02 +0200

    updated

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -1,58 +1,87 @@
 #!/usr/bin/env python3
+"""
+    Neural style transfer
+"""
 
 import numpy as np
 import tensorflow as tf
 
+
 class NST:
-    # Define the style and content layers as class attributes
-    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
+    """
+        Class that performs tasks for neural style transfer
+    """
+
+    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
+                    'block4_conv1', 'block5_conv1']
     content_layer = 'block5_conv2'
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
-        # Validate inputs
-        if not isinstance(style_image, np.ndarray) or style_image.shape[-1] != 3:
-            raise TypeError("style_image must be a numpy.ndarray with shape (h, w, 3)")
-        if not isinstance(content_image, np.ndarray) or content_image.shape[-1] != 3:
-            raise TypeError("content_image must be a numpy.ndarray with shape (h, w, 3)")
+        """
+            Class constructor neural style transfer
+
+            :param style_image: ndarray, image used as style reference
+            :param content_image: ndarray, image used as content reference
+            :param alpha: weight for content cost
+            :param beta: weight for style cost
+        """
+
+        self.style_image = style_image
+
+        if (not isinstance(style_image, np.ndarray)
+                or style_image.shape[-1] != 3):
+            raise TypeError("style_image must be a numpy.ndarray"
+                            " with shape (h, w, 3)")
+        else:
+            self.style_image = self.scale_image(style_image)
+        if (not isinstance(content_image, np.ndarray)
+                or content_image.shape[-1] != 3):
+            raise TypeError("content_image must be a numpy.ndarray"
+                            " with shape (h, w, 3)")
+        else:
+            self.content_image = self.scale_image(content_image)
         if not isinstance(alpha, (int, float)) or alpha < 0:
             raise TypeError("alpha must be a non-negative number")
+        else:
+            self.alpha = alpha
         if not isinstance(beta, (int, float)) or beta < 0:
             raise TypeError("beta must be a non-negative number")
-
-        # Preprocess the images
-        self.style_image = self.scale_image(style_image)
-        self.content_image = self.scale_image(content_image)
-        self.alpha = alpha
-        self.beta = beta
+        else:
+            self.beta = beta
 
     @staticmethod
     def scale_image(image):
-        # Validate the image input
+        """
+            rescales an image such that its pixels values are between 0 and 1
+            and its largest side is 512 px
+
+            :param image: ndarray, shape(h,w,3) image to be scaled
+
+            :return:scaled image
+        """
         if not isinstance(image, np.ndarray) or image.shape[-1] != 3:
-            raise TypeError("image must be a numpy.ndarray with shape (h, w, 3)")
+            raise (TypeError
+                   ("image must be a numpy.ndarray with shape (h, w, 3)"))
 
-        # Normalize the image to range [0, 1]
-        image = image / 255.0
+        h, w, _ = image.shape
 
-        # Convert the image to a TensorFlow tensor and add a batch dimension
-        image = tf.convert_to_tensor(image, dtype=tf.float32)
-        image = tf.expand_dims(image, axis=0)
-
-        # Get the height and width of the image
-        h, w, _ = image.shape[1:]
-
-        # Calculate the new height and width to maintain aspect ratio
-        if h > w:
-            new_h = 512
-            new_w = int(w * 512 / h)
+        if w > h:
+            w_new = 512
+            h_new = int((h * 512) / w)
         else:
-            new_w = 512
-            new_h = int(h * 512 / w)
+            h_new = 512
+            w_new = int((w * 512) / h)
 
-        # Resize the image using bicubic interpolation
-        resized_image = tf.image.resize(image, (new_h, new_w), method=tf.image.ResizeMethod.BICUBIC)
+        resized_image = tf.image.resize(image,
+                                        size=[h_new, w_new],
+                                        method='bicubic')
 
-        # Ensure pixel values are clipped to range [0, 1]
-        resized_image = tf.clip_by_value(resized_image, 0.0, 1.0)
+        # Normalize
+        resized_image = resized_image / 255
 
-        return resized_image
+        # limit pixel value between 0 and 1
+        resized_image = tf.clip_by_value(resized_image, 0, 1)
+
+        tf_resize_image = tf.expand_dims(resized_image, 0)
+
+        return tf_resize_image
