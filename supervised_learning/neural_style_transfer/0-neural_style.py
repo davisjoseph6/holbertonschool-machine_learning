828dda376c599fec4149a2dc59e02bd6c72e78ab
davisjoseph6 <davisjoseph767@email.com>
2024-07-05 11:51:38 +0200

    updated

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-    Neural style transfer
+Neural style transfer
 """
 
 import numpy as np
@@ -9,7 +9,7 @@ import tensorflow as tf
 
 class NST:
     """
-        Class that performs tasks for neural style transfer
+    Class that performs tasks for neural style transfer
     """
 
     style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
@@ -18,32 +18,28 @@ class NST:
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
         """
-            Class constructor neural style transfer
-
-            :param style_image: ndarray, image used as style reference
-            :param content_image: ndarray, image used as content reference
-            :param alpha: weight for content cost
-            :param beta: weight for style cost
+        Class constructor neural style transfer
         """
-
-        self.style_image = style_image
 
         if (not isinstance(style_image, np.ndarray)
                 or style_image.shape[-1] != 3):
             raise TypeError("style_image must be a numpy.ndarray"
-                            " with shape (h, w, 3)")
+                    " with shape (h, w, 3)")
         else:
             self.style_image = self.scale_image(style_image)
+
         if (not isinstance(content_image, np.ndarray)
                 or content_image.shape[-1] != 3):
             raise TypeError("content_image must be a numpy.ndarray"
-                            " with shape (h, w, 3)")
+                    " with shape (h, w, 3)")
         else:
             self.content_image = self.scale_image(content_image)
+
         if not isinstance(alpha, (int, float)) or alpha < 0:
             raise TypeError("alpha must be a non-negative number")
         else:
             self.alpha = alpha
+
         if not isinstance(beta, (int, float)) or beta < 0:
             raise TypeError("beta must be a non-negative number")
         else:
@@ -52,16 +48,10 @@ class NST:
     @staticmethod
     def scale_image(image):
         """
-            rescales an image such that its pixels values are between 0 and 1
-            and its largest side is 512 px
-
-            :param image: ndarray, shape(h,w,3) image to be scaled
-
-            :return:scaled image
+        rescales an image such that its pixel values are between 0 and 1 and its largest side is 512 px
         """
         if not isinstance(image, np.ndarray) or image.shape[-1] != 3:
-            raise (TypeError
-                   ("image must be a numpy.ndarray with shape (h, w, 3)"))
+            raise TypeError("image must be a numpy.ndarray with shape (h, w, 3)")
 
         h, w, _ = image.shape
 
@@ -73,13 +63,13 @@ class NST:
             w_new = int((w * 512) / h)
 
         resized_image = tf.image.resize(image,
-                                        size=[h_new, w_new],
-                                        method='bicubic')
+                size=[h_new, w_new],
+                method='bicubic')
 
         # Normalize
-        resized_image = resized_image / 255
+        resized_image = resized_image / 255.0
 
-        # limit pixel value between 0 and 1
+        # Limit pixel values between 0 and 1
         resized_image = tf.clip_by_value(resized_image, 0, 1)
 
         tf_resize_image = tf.expand_dims(resized_image, 0)
