cb8959c3ccee83cdaffc379267fb25bfc8e01957
davisjoseph6 <davisjoseph767@email.com>
2024-07-05 11:59:23 +0200

    updated

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -24,14 +24,14 @@ class NST:
         if (not isinstance(style_image, np.ndarray)
                 or style_image.shape[-1] != 3):
             raise TypeError("style_image must be a numpy.ndarray"
-                    " with shape (h, w, 3)")
+                            " with shape (h, w, 3)")
         else:
             self.style_image = self.scale_image(style_image)
 
         if (not isinstance(content_image, np.ndarray)
                 or content_image.shape[-1] != 3):
             raise TypeError("content_image must be a numpy.ndarray"
-                    " with shape (h, w, 3)")
+                            " with shape (h, w, 3)")
         else:
             self.content_image = self.scale_image(content_image)
 
@@ -48,10 +48,12 @@ class NST:
     @staticmethod
     def scale_image(image):
         """
-        rescales an image such that its pixel values are between 0 and 1 and its largest side is 512 px
+        rescales an image such that its pixel values are between 0 and 1
+        and its largest side is 512 px
         """
         if not isinstance(image, np.ndarray) or image.shape[-1] != 3:
-            raise TypeError("image must be a numpy.ndarray with shape (h, w, 3)")
+            raise (TypeError
+                   ("image must be a numpy.ndarray with shape (h, w, 3)"))
 
         h, w, _ = image.shape
 
@@ -63,8 +65,8 @@ class NST:
             w_new = int((w * 512) / h)
 
         resized_image = tf.image.resize(image,
-                size=[h_new, w_new],
-                method='bicubic')
+                                        size=[h_new, w_new],
+                                        method='bicubic')
 
         # Normalize
         resized_image = resized_image / 255.0
