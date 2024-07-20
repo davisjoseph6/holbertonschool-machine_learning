27245b1bb10cc943a3cd2ce70316522616b0f3bd
davisjoseph6 <davisjoseph767@email.com>
2024-07-20 14:10:52 +0200

    updated

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -8,14 +8,21 @@ def sub_matrix(matrix, i):
     """
     Creates a submatrix by removing the first row and the i-th column
     """
-    return [row[:i] + row[i + 1:] for row in matrix[1:]]
+    if not matrix:
+        return []
+
+    matrix2 = []
+    for row in matrix[1:]:  # Skip the first row
+        matrix2.append(row[:i] + row[i + 1:])  # Remove the i-th column
+
+    return matrix2
 
 
 def determinant(matrix):
     """
     Calculates the determinant of a matrix.
     """
-    # Test if matrix is a list of lists
+    # test if matrix is a list of lists
     if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
     for sub_list in matrix:
@@ -26,9 +33,9 @@ def determinant(matrix):
     if len(matrix[0]) == 0:
         return 1
 
-    # Test if matrix is square
+    # test if matrix is square
     if len(matrix) != len(matrix[0]):
-        raise ValueError("matrix must be a non-empty square matrix")
+        raise ValueError("matrix must be a square matrix")
 
     # Base cases for small matrices
     if len(matrix) == 1:
@@ -39,7 +46,11 @@ def determinant(matrix):
     # Recursive case: Calculate the determinant using cofactor expansion
     det = 0
     for i in range(len(matrix[0])):
-        det += ((-1) ** i) * matrix[0][i] * determinant(sub_matrix(matrix, i))
+        det += (
+                ((-1) ** i) * matrix[0][i] * determinant(
+                    sub_matrix(matrix, i)
+                    )
+                )
     return det
 
 
