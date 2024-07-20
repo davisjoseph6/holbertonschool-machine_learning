68359f01351a8a000204bfe08f283f4960b12581
davisjoseph6 <davisjoseph767@email.com>
2024-07-20 15:14:14 +0200

    dupated

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -1,36 +1,26 @@
 #!/usr/bin/env python3
 """
-    Minor
+Definiteness
 """
+
 import numpy as np
 
 
 def definiteness(matrix):
     """
-        function that calculates the definiteness of a matrix
-
-        :param matrix: ndarray, shape(n,n)
-
-        :return: - Positive definite
-                 - Positive semi-definite
-                 - Negative semi-definite
-                 - Negative definite
-                 - Indefinite
+    Calculates the definiteness of a matrix.
     """
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
     if matrix.size == 0:
         return None
-    # test square matrix
     if matrix.shape[0] != matrix.shape[1]:
         return None
-    # check matrix symetric
     if not np.array_equal(matrix, matrix.T):
         return None
 
     eigen_val, _ = np.linalg.eig(matrix)
 
-    # determin min and max eigen values
     min_eig = np.min(eigen_val)
     max_eig = np.max(eigen_val)
 
@@ -44,3 +34,5 @@ def definiteness(matrix):
         return "Negative semi-definite"
     elif min_eig < 0 < max_eig:
         return "Indefinite"
+    else:
+        return None
