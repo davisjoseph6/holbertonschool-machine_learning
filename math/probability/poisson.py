d60b52548e19c7e69949479a74923b8fc11bb151
davisjoseph6 <davisjoseph767@email.com>
2024-07-21 09:36:08 +0200

    updated

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -24,15 +24,15 @@ class Poisson:
             self.lambtha = float(sum(data) / len(data))
 
 
-def pmf(self, k):
-    """
-    Calculates the value of the PMF for a given number of successes.
-    """
-    k = int(k)
-    if k < 0:
-        return 0
-    # Using the PMF formula: P(k; λ) = (λ^k * e^(-λ)) / k!
-    factorial = 1
-    for i in range(1, k + 1):
-        factorial *= i
-    return (self.lambtha ** k * 2.7182818285 ** (-self.lambtha)) / factorial
+    def pmf(self, k):
+        """
+        Calculates the value of the PMF for a given number of successes.
+        """
+        k = int(k)
+        if k < 0:
+            return 0
+        # Using the PMF formula: P(k; λ) = (λ^k * e^(-λ)) / k!
+        factorial = 1
+        for i in range(1, k + 1):
+            factorial *= i
+        return (self.lambtha ** k * 2.7182818285 ** (-self.lambtha)) / factorial
