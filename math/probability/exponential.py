265c4d7764a9bb4dda8fdea3144014745b0f471f
davisjoseph6 <davisjoseph767@email.com>
2024-07-21 12:08:18 +0200

    updated

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -31,3 +31,11 @@ class Exponential:
         if x < 0:
             return 0
         return self.lambtha * 2.7182818285 ** (-self.lambtha * x)
+
+    def cdf(self, x):
+        """
+        Calcualtes the value of the CDF for a given time period.
+        """
+        if x < 0:
+            return 0
+        return 1 - 2.7182818285 ** (-self.lambtha * x)
