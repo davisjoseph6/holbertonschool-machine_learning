6d0110e754e04f18cea3c0c299418c733f2dc7e5
davisjoseph6 <davisjoseph767@email.com>
2024-07-25 14:34:01 +0200

    updated

--- math/bayesian_prob/100-continuous.py
+++ math/bayesian_prob/100-continuous.py
@@ -14,7 +14,8 @@ def posterior(x, n, p1, p2):
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+                "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not(isinstance(p1, float) and 0 <= p1 <= 1):
@@ -28,7 +29,8 @@ def posterior(x, n, p1, p2):
     alpha = x + 1
     beta = n - x + 1
 
-    # Calculate the cumulative density function (CDF) for Beta distribution at p1 and p2
+    # Calculate the cumulative density function (CDF)
+    # for Beta distribution at p1 and p2
     cdf_p1 = special.btdtr(alpha, beta, p1)
     cdf_p2 = special.btdtr(alpha, beta, p2)
 
