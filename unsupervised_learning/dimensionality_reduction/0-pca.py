beec16c1d4d9eedbcee778d59bb3ca92de3224b6
davisjoseph6 <davisjoseph767@email.com>
2024-07-27 21:29:56 +0200

    updated

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-    PCA on a dataset
+PCA on a dataset
 """
 import numpy as np
 
@@ -8,18 +8,7 @@ import numpy as np
 def pca(X, var=0.95):
     """
     Function that performs PCA on a dataset
-
-    :param X: numpy.ndarray of shape (n, d) where:
-        - n is the number of data points
-        - d is the number of dimensions in each point
-        - all dimensions have a mean of 0 across all data points
-    :param var: the  fraction of the variance that the PCA transformation
-        should maintain
-
-    :return: the weights matrix, W,
-        that maintains var fraction of X‘s original variance
     """
-
     # Calculate the SVD of input data
     U, S, V = np.linalg.svd(X, full_matrices=False)
 
@@ -29,7 +18,7 @@ def pca(X, var=0.95):
     # Determine the number of components to keep
     nb_comp = np.argmax(var_ratio >= var) + 1
 
-    # select first nb_comp
-    W = V[:nb_comp + 1].T
+    # Select first nb_comp components
+    W = V[:nb_comp].T
 
     return W
