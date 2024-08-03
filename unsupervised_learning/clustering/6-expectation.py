8f876205d0dbf64fd920155619d1b0efa17227a3
davisjoseph6 <davisjoseph767@email.com>
2024-08-03 18:11:34 +0200

    updated

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -11,16 +11,13 @@ def expectation(X, pi, m, S):
     """
     Calculates the expectation step in the EM algorithm for a GMM.
     """
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
-        return None, None
-    if not isinstance(pi, np.ndarray) or pi.ndim != 1:
-        return None, None
-    if not isinstance(m, np.ndarray) or m.ndim != 2:
-        return None, None
-    if not isinstance(S, np.ndarray) or S.ndim != 3:
-        return None, None
-    if (X.shape[1] != m.shape[1] or S.shape[1] != S.shape[2] or
-            m.shape[1] != S.shape[1] or pi.shape[0] != m.shape[0]):
+    if (not isinstance(X, np.ndarray) or X.ndim != 2 or
+            not isinstance(pi, np.ndarray) or pi.ndim != 1 or
+            not isinstance(m, np.ndarray) or m.ndim != 2 or
+            not isinstance(S, np.ndarray) or S.ndim != 3 or
+            X.shape[1] != m.shape[1] or m.shape[0] != S.shape[0] or
+            S.shape[1] != S.shape[2] or
+            pi.shape[0] != m.shape[0] or pi.shape[0] != S.shape[0]):
         return None, None
 
     if not np.isclose([np.sum(pi)], [1])[0]:
