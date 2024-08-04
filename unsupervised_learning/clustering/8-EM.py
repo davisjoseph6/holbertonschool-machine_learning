d04449205df8bad77e2cb268df7ea8a786ad4a91
davisjoseph6 <davisjoseph767@email.com>
2024-08-04 11:14:35 +0200

    updated

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -13,35 +13,40 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     """
     Performs the expectation maximization for a GMM.
     """
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
-        return None, None, None, None, None
-    if not isinstance(k, int) or k <= 0:
-        return None, None, None, None, None
-    if not isinstance(iterations, int) or iterations <= 0:
-        return None, None, None, None, None
-    if not isinstance(tol, float) or tol < 0:
-        return None, None, None, None, None
-    if not isinstance(verbose, bool):
+    if (
+            not isinstance(X, np.ndarray) or X.ndim != 2
+            or not isinstance(k, int) or k <= 0
+            or not isinstance(iterations, int) or iterations <= 0
+            or not isinstance(tol, float) or tol < 0
+            or not isinstance(verbose, bool)
+            ):
+
         return None, None, None, None, None
 
+    # Initialize priors, centroid means, and covariance matrices
     pi, m, S = initialize(X, k)
-    g, l = expectation(X, pi, m, S)
 
     for i in range(iterations):
-        pi, m, S = maximization(X, g)
-        g, new_l = expectation(X, pi, m, S)
+        # Evaluate the probabilities and likelihoods with current parameters
+        g, prev_li = expectation(X, pi, m, S)
 
+        # In verbose mode, print the likelihood every 10 iterations after 0
         if verbose and i % 10 == 0:
-            print(f'Log Likelihood after {i} iterations: {l:.5f}')
+            print(f"Log Likelihood after {i} iterations: {round(prev_li, 5)}")
 
-        if abs(new_l - l) <= tol:
-            if verbose:
-                print(f'Log Likelihood after {i} iterations: {new_l:.5f}')
+        # Re-estimate the parameters with the new values
+        pi, m, S = maximization(X, g)
+
+        # Evaluate new log likelihood
+        g, li = expectation(X, pi, m, S)
+
+        # If the likelihood varied by less than the tolerance value, we stop
+        if np.abs(li - prev_li) <= tol:
             break
 
-        l = new_l
-
+    # Last verbose message with current likelihood
     if verbose:
-        print(f'Log Likelihood after {i} iterations: {l:.5f}')
+        # NOTE i + 1 since it has been updated once more since last print
+        print(f"Log Likelihood after {i + 1} iterations: {round(li, 5)}")
 
-    return pi, m, S, g, l
+    return pi, m, S, g, li
