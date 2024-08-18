17f978ad85f489f53ac782182f1e170bf5d60d1b
davisjoseph6 <davisjoseph767@email.com>
2024-08-18 15:52:43 +0200

    updated

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -6,20 +6,19 @@ Module documentation for '0-gp'.
 import numpy as np
 
 
-
 class GaussianProcess:
     """
     Class that represents a noiseless 1D Gaussian process.
     """
 
-    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+    def __init__(self, X_init, Y_init, length_scale=1, sigma_f=1):
         """
         Class constructor.
         """
 
         self.X = X_init
         self.Y = Y_init
-        self.l = l
+        self.length_scale = length_scale
         self.sigma_f = sigma_f
         self.K = self.kernel(X_init, X_init)
 
@@ -29,5 +28,5 @@ class GaussianProcess:
         the Radial Basis Function (RBF) kernel.
         """
         sqdist = np.sum(X1**2, 1).reshape(-1, 1) + \
-                 np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
-        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
+            np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        return self.sigma_f**2 * np.exp(-0.5 / self.length_scale**2 * sqdist)
