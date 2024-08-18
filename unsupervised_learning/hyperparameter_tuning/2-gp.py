c227930a3619d988ae0c9fa564c3fdc6844ad068
davisjoseph6 <davisjoseph767@email.com>
2024-08-18 18:56:54 +0200

    updated

--- unsupervised_learning/hyperparameter_tuning/2-gp.py
+++ unsupervised_learning/hyperparameter_tuning/2-gp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module documentation for '1-gp'.
+Module documentation for '2-gp'.
 """
 
 import numpy as np
@@ -53,3 +53,24 @@ class GaussianProcess:
         sigma_s_diag = np.diag(sigma_s)
 
         return mu_s, sigma_s_diag
+
+    def update(self, X_new, Y_new):
+        """
+        Updates the Gaussian Process with a new sample point.
+        """
+        # Update X and Y with the new data point
+        self.X = np.vstack((self.X, X_new.reshape(-1, 1)))
+        self.Y = np.vstack((self.Y, Y_new.reshape(-1, 1)))
+
+        # Calculate the covariance between the new point and all previous
+        # points
+        K_s = self.kernel(self.X, X_new.reshape(-1, 1))
+
+        # Update the K matrix with the new row and column
+        K_new_row = K_s[:-1, :]
+        K_new_entry = K_s[-1, :].reshape(1, -1)
+
+        self.K = np.block([
+            [self.K, K_new_row],
+            [K_new_row.T, K_new_entry]
+            ])
