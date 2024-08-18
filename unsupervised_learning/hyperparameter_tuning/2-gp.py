f1160c51576d24862bdb863fefd731cb270cabec
davisjoseph6 <davisjoseph767@email.com>
2024-08-18 19:02:27 +0200

    added

--- unsupervised_learning/hyperparameter_tuning/2-gp.py
+++ unsupervised_learning/hyperparameter_tuning/2-gp.py
@@ -70,7 +70,9 @@ class GaussianProcess:
         K_new_row = K_s[:-1, :]
         K_new_entry = K_s[-1, :].reshape(1, -1)
 
-        self.K = np.block([
+        self.K = np.block(
+                [
             [self.K, K_new_row],
             [K_new_row.T, K_new_entry]
-            ])
+            ]
+                )
