f4bfe85ea0f255d1a2dabc20471ee10e413f2608
davisjoseph6 <davisjoseph767@email.com>
2024-08-18 16:15:44 +0200

    updated

---
+++ unsupervised_learning/hyperparameter_tuning/1-gp.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Module documentation for '1-gp'.
+"""
+
+import numpy as np
+
+
+class GaussianProcess:
+    """
+    Class that represents a noiseless 1D Gaussian process.
+    """
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """
+        Class constructor.
+        """
+
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(X_init, X_init)
+
+    def kernel(self, X1, X2):
+        """
+        Calculates the covariance kernel matrix between two matrices using
+        the Radial Basis Function (RBF) kernel.
+        """
+        sqdist = np.sum(X1**2, 1).reshape(-1, 1) + \
+            np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
+
+    def predict(self, X_s):
+        """
+        Predicts the mean and standard deviation of points in a
+        Gaussian process.
+        """
+        # Calculate the covariance between X_s and X
+        K_s = self.kernel(X_s, self.X)
+        # Calculate the covariance matrix of the points in X_s
+        K_ss = self.kernel(X_s, X_s)
+        # Calculate the inverse of the covariance matrix K
+        K_inv = np.linalg.inv(self.K)
+
+        # Compute the mean vector mu for the points in X_s
+        mu_s = K_s.dot(K_inv).dot(self.Y).flatten()
+
+        # Compute the covariance matrix Sigma for the points in X_s
+        sigma_s = K_ss - K_s.dot(K_inv).dot(K_s.T)
+
+        # The variance is the diagonal of the covariance matrix
+        sigma_s_diag = np.diag(sigma_s)
+
+        return mu_s, sigma_s_diag
