598b61f34319953b420a523adebf16a77bda1d49
davisjoseph6 <davisjoseph767@email.com>
2024-08-23 03:43:39 +0200

    updated

--- unsupervised_learning/autoencoders/2-convolutional.py
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -45,12 +45,9 @@ def autoencoder(input_dims, filters, latent_dims):
         x = keras.layers.Conv2D(f, (3, 3), activation='relu', padding='same')(x)
         x = keras.layers.UpSampling2D((2, 2))(x)
 
-    # Conv2D and UpSampling layers to adjust the dimensions correctly
-    x = keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same')(x)
-    x = keras.layers.Conv2D(16, (3, 3), activation='relu', padding='valid')(x)
+    # Final two Conv2D layers to adjust the dimensions and filters
+    x = keras.layers.Conv2D(filters[-1], (3, 3), activation='relu', padding='valid')(x)
     x = keras.layers.UpSampling2D((2, 2))(x)
-
-    # Final output layer to match the expected output shape
     outputs = keras.layers.Conv2D(input_dims[-1], (3, 3), activation='sigmoid', padding='same')(x)
 
     # Models
