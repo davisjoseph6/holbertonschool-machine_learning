f53da144de30c1bdd6cc0bfcaaaebac62a40e988
davisjoseph6 <davisjoseph767@email.com>
2024-08-31 06:39:32 +0200

    updated

--- unsupervised_learning/gan/3-generate_faces.py
+++ unsupervised_learning/gan/3-generate_faces.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
+"""
+Module 3-generate_faces
+"""
 
 import tensorflow as tf
 from tensorflow import keras
 
 
 def convolutional_GenDiscr():
+    """
+    Builds and returns the generator and discriminator models for a GAN.
+    """
     def generator():
+        """
+        Creates and returns the generator model.
+        """
         model = keras.Sequential([
             keras.layers.Input(shape=(16,)),
             keras.layers.Dense(2048, activation='tanh'),
@@ -26,6 +35,9 @@ def convolutional_GenDiscr():
         return model
 
     def discriminator():
+        """
+        Creates and returns the discriminator model.
+        """
         model = keras.Sequential([
             keras.layers.Input(shape=(16, 16, 1)),
             keras.layers.Conv2D(32, (3, 3), padding="same"),
