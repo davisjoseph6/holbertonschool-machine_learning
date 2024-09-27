b17731224b1d5fdbc925d21b7b5b5ba29e7b606e
davisjoseph6 <davisjoseph767@email.com>
2024-09-28 01:33:39 +0200

    updated

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -13,7 +13,8 @@ class Encoder(tf.keras.layers.Layer):
     Encoder class that inherits from TensorFlow's Keras Layer.
     """
 
-    def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len, drop_rate=0.1):
+    def __init__(self, N, dm, h, hidden, input_vocab,
+                 max_seq_len, drop_rate=0.1):
         """
         Initializes the Encoder.
         """
@@ -22,13 +23,16 @@ class Encoder(tf.keras.layers.Layer):
         self.dm = dm
 
         # Embedding layer
-        self.embedding = tf.keras.layers.Embedding(input_dim=input_vocab, output_dim=dm)
+        self.embedding = tf.keras.layers.Embedding(input_dim=input_vocab,
+                                                   output_dim=dm)
 
         # Positional encoding
         self.positional_encoding = positional_encoding(max_seq_len, dm)
 
         # Encoder blocks
-        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate) for _ in range(N)]
+        self.blocks = [
+                EncoderBlock(dm, h, hidden, drop_rate) for _ in range(N)
+                ]
 
         # Dropout layer
         self.dropout = tf.keras.layers.Dropout(rate=drop_rate)
@@ -42,7 +46,8 @@ class Encoder(tf.keras.layers.Layer):
         x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))  # Scale embeddings
         x += self.positional_encoding[:seq_len]
 
-        x = self.dropout(x, training=training)  # Apply dropout to positional encoding
+        # Apply dropout to positional encoding
+        x = self.dropout(x, training=training)
 
         for block in self.blocks:
             x = block(x, training, mask)  # Pass through each EncoderBlock
