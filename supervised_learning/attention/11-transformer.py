5b98ae153c06af4a55d57143513fe8cfaa1992bc
davisjoseph6 <davisjoseph767@email.com>
2024-09-28 02:29:19 +0200

    fixed pycodestyle

--- supervised_learning/attention/11-transformer.py
+++ supervised_learning/attention/11-transformer.py
@@ -13,21 +13,35 @@ class Transformer(tf.keras.Model):
     Transformer class inheriting from TensorFlow's Keras Model.
     """
 
-    def __init__(self, N, dm, h, hidden, input_vocab, target_vocab, max_seq_input, max_seq_target, drop_rate=0.1):
+    def __init__(self, N, dm, h, hidden, input_vocab, target_vocab,
+                 max_seq_input, max_seq_target, drop_rate=0.1):
         """
         Initializes the Transformer model.
         """
         super(Transformer, self).__init__()
-        self.encoder = Encoder(N, dm, h, hidden, input_vocab, max_seq_input, drop_rate)
-        self.decoder = Decoder(N, dm, h, hidden, target_vocab, max_seq_target, drop_rate)
+        self.encoder = Encoder(N, dm, h, hidden, input_vocab,
+                               max_seq_input, drop_rate)
+        self.decoder = Decoder(N, dm, h, hidden, target_vocab,
+                               max_seq_target, drop_rate)
         self.linear = tf.keras.layers.Dense(units=target_vocab)
 
-    def call(self, inputs, target, training, encoder_mask, look_ahead_mask, decoder_mask):
+    def call(self, inputs, target, training, encoder_mask,
+             look_ahead_mask, decoder_mask):
         """
         Forward pass for the Transformer model.
         """
-        encoder_output = self.encoder(inputs, training, encoder_mask)  # Encoder output
-        decoder_output = self.decoder(target, encoder_output, training, look_ahead_mask, decoder_mask)  # Decoder output
+        # Encoder output
+        encoder_output = self.encoder(inputs,
+                                      training,
+                                      encoder_mask)
+
+        # Decoder output
+        decoder_output = self.decoder(target,
+                                      encoder_output,
+                                      training,
+                                      look_ahead_mask,
+                                      decoder_mask)
+
         output = self.linear(decoder_output)  # Final linear transformation
 
         return output
