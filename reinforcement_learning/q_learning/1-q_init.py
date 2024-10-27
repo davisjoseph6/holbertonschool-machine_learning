0ac10de8a3186c55cc4c248864d54b01d4eb370c
davisjoseph6 <davisjoseph6@gmail.com>
2024-10-27 21:02:43 +0100

    updated

---
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module to initialize the Q-table for a FrozenLake environment.
+"""
+
+import numpy as np
+
+def q_init(env):
+    """
+    Initializes the Q-table as a numpy array of zeros.
+    """
+    num_states = env.observation_space.n
+    num_actions = env.action_space.n
+    Q_table = np.zeros((num_states, num_actions))
+    return Q_table
