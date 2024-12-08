56cde4d0f7b59e4fedd5284844402209d5170ed2
davisjoseph6 <davisjoseph767@gmail.com>
2024-12-08 01:51:03 +0100

    updated

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -6,6 +6,7 @@ Module to concatenate two pandas DataFrames with specific conditions.
 import pandas as pd
 index = __import__('10-index').index
 
+
 def concat(df1, df2):
     """
     Concatenates two DataFrames with indexing and specific conditions.
@@ -20,11 +21,12 @@ def concat(df1, df2):
     # Set Timestamp as index for both DataFrames
     df1 = index(df1)
     df2 = index(df2)
-    
+
     # Filter df2 to include rows with Timestamp <= 1417411920
     df2_filtered = df2[df2.index <= 1417411920]
-    
+
     # Concatenate df2_filtered above df1 with keys for labeling
-    concatenated = pd.concat([df2_filtered, df1], keys=["bitstamp", "coinbase"])
-    
+    concatenated = pd.concat([df2_filtered, df1], keys=["bitstamp", "coinbase"]
+                             )
+
     return concatenated
